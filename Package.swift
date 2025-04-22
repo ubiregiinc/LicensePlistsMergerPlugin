66bfb83df0352c637ebadd9e3f5ecd179802b2c7
trickart <ryotaro.seki@ubiregi.com>
2025-04-22 15:34:38 +0900

    Add impl

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LicensePlistsMergerPlugin",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .plugin(
+            name: "LicensePlistsMergerPlugin",
+            targets: ["LicensePlistsMergerPlugin"]),
+    ],
+    targets: [
+        .plugin(
+            name: "LicensePlistsMergerPlugin",
+            capability: .command(
+                intent: .custom(verb: "license-plists-merger", description: "merge license plists"),
+                permissions: [
+                    .writeToPackageDirectory(reason: "output license files.")
+                ]
+            ),
+            dependencies: [.target(name: "license-plists-merger")]
+        ),
+        .binaryTarget(name: "license-plists-merger", url: "https://github.com/ubiregiinc/LicensePlistsMerger/releases/download/0.3.0/license-plists-merger-macos.artifactbundle.zip", checksum: "cc59121e5b89203697f4e9a620a1988206a09292dd383916fb109e735a1acbcb"),
+    ]
+)
